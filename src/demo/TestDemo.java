fdbd9b3375c20c5647945cc8ecb279cf268a7a3a
Cree <Cree@cristians-air.attlocal.net>
2024-01-06 14:39:24 -0800

    DDDDDD

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -7,4 +7,14 @@ public class TestDemo {
 		System.out.println("Something");
 		System.out.println("One more line of code to local");
 	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	public void subtract(int a, int b) {
+		System.out.println(a-b);
+	}
+	public void divide(int a, int b) {
+		System.out.println(a/b);
+	}
 }
